3dfd99c4fa5480272f2d8b97218133ea3c71ab9e
Charles <Charleskojomark>
2024-08-03 11:22:52 +0100

    skeleton commit

---
+++ app.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const mongoose = require('mongoose');
+
+require('dotenv').config();
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(express.json());
+
+mongoose.connect(process.env.MONGODB_URI)
+.then(()=>{
+    console.log("Connected to DB");
+}).catch((err)=>{
+    console.error("Failed to connect to DB",err);
+})
+
+app.listen(PORT,()=>{
+    console.log(`Server running on ${PORT}`);
+})
+
